34a3f572086f0ee2a0f801bd9f12585df879e498
vidyatiwrai <127404171+vidyatiwrai@users.noreply.github.com>
2023-04-23 14:28:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// your code here
+
+let name = document.getElementById("name").value;
+let year = document.getElementById("year").value;
+let h3 = document.getElementById("url");
+
+document.getElementById("button").addEventListener("click", callbutton) 
+
+
+function callbutton() {
+
+	  let h3 = document.createElement("h3");
+	let Name = document.createElement("h3");
+	Name.innerText = "name" + name;
+
+	let Year = document.createElement("h3");
+	year.innerText = "year" + year;
+
+	h3.append(Name);
+	h3.append(Year);
+
+	document.querySelector("#url").append(h3);
+	
+}
